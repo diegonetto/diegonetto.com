aa7511913af0ee59f8aa16bf96b39e6551e538d8
Diego Netto <diegormnetto@gmail.com>
2012-08-30 17:31:12 -0400

    Added hostname for production and development environments.

--- app.js
+++ app.js
@@ -22,12 +22,21 @@ app.configure(function(){
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
+// development only
 app.configure('development', function(){
+  console.log('This is a development environment');
   app.use(express.errorHandler());
+  app.set('hostname', 'localhost');
+});
+
+// production only
+app.configure('production', function(){
+  console.log('This is a production environment');
+  app.set('hostname', 'diegonetto.com');
 });
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), app.get('hostname'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
