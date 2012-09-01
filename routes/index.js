0f5500e19f7efc634370bc3dae2d5ca6467cfdfd
Diego Netto <diegormnetto@gmail.com>
2012-09-01 00:50:59 -0400

    Implemented pulling of tweets and rendering for home page view.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,33 @@
+/**
+ * Dependencies
+ */
+var Db = require('mongodb').Db,
+  Connection = require('mongodb').Connection,
+  Server = require('mongodb').Server;
+
+/**
+ * Constants
+ */
+var HOST = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var PORT = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
+
+/**
+ * 
+ */
 
 /*
  * GET home page.
  */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  var db = new Db('diegonetto', new Server(HOST, PORT, {}));
+
+  db.open(function(err, db) {
+    db.collection('tweets', function(err, collection) {
+      collection.find().toArray(function(err, results) {
+        res.render('index', { title: 'Diego Netto', tweets: results });
+        db.close();
+      });
+    });
+  });
 };
+
