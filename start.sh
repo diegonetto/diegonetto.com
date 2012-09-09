0e5f5f5271a17f4807a84af9da008ac51b3a6573
Diego Netto <diegormnetto@gmail.com>
2012-09-09 18:54:43 -0400

    Added append switch to start script.

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-forever -l diegonetto.com.log -o /home/ubuntu/.forever/diegonetto.com/out.log -e /home/ubuntu/.forever/diegonetto.com/err.log start diegonetto.com.js
+forever -a -l diegonetto.com.log -o /home/ubuntu/.forever/diegonetto.com/out.log -e /home/ubuntu/.forever/diegonetto.com/err.log start diegonetto.com.js
 
